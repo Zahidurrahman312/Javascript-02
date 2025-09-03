2f4e968282fae8bbd6806c9e5be2de775857a284
unknown <zahidrehamn886554@gmail.com>
2025-09-03 07:57:03 +0500

    complete

---
+++ app.js
@@ -0,0 +1,227 @@
+console.log("JavaScript connected!");
+
+// Q-1 craet two varaible numbers and them add in a new varaible and show the browser
+
+// Answer :
+
+// var numm1 = 3;
+
+// var num2 = 5;
+
+// var sum = numm1 + num2;
+
+// document.write("Sum of " + numm1 +" and " + num2 + " is " + sum);
+
+// Q-2 Repeat the task1 for substraction multiplication division and modulus?
+
+// Answer :
+
+// var num1 = 3;
+
+// var num2 = 5;
+
+// var substraction = num1-num2;
+
+// console.log(substraction);
+
+// var num1 = 3;
+
+// var num2 = 5;
+
+// var multiplication = num1*num2;
+
+// console.log(multiplication);
+
+// // var num1 = 3;
+
+// var num2 = 5;
+
+// var division = num1/ num2;
+
+// console.log(division);
+
+// var num1 = 3;
+
+// var num2 = 5;
+
+// var reminder = num2 % num1
+
+// console.log( reminder);
+
+// Q-03 Do the following methimatic expression use js 
+
+// Part(a) Declear a variable
+
+// var age;
+
+// console.log(age);
+
+// Part(b) show the value of variable in your browser
+
+// var keywords = "value after the decleration is that ";
+
+// document.write(keywords);
+
+// Part(c) initialize the variable with some number 
+
+// var num = 25;
+
+// console.log(num);
+
+// Part(d) show the initialize value in your browser
+
+// var num = 25;
+
+// document.write(num);
+
+// Part(e) increment the value
+
+// var x = 5; 
+
+// ++x;
+
+// console.log(x);
+
+// // Part(d) show the increment value in browser
+
+// document.write("After the value of increment: " + x +" <br>" );
+
+// // Part(e) add 7 with increment
+
+// var y = 7;
+
+// var sum = x + y;
+
+// document.write("After the addition: " + sum );
+
+// Part(i) Decrement variable 
+
+// var x =13;
+
+// x--;
+// document.write("Decremnet of variable: " + x)
+
+// ______Part(d)______
+
+// var x=12;
+
+// var y =3;
+// var z =x % y;
+// document.write("The reminder is: " + z);
+
+// ______Q-04_______
+
+// var teckite =600;
+
+// var buying =5;
+
+// var totalPrice= teckite*buying;
+
+// document.write("Total cost to buy 5 teckites to a movive is "+totalPrice +("PKR"))
+
+// _____Q-06________
+
+// var celuise =25;
+
+// var fornhite=(celuise*9/5)+32;
+// document.write(celuise+"ºc is "+fornhite+("ºF"));
+
+// var fornhit=70;
+
+// var celuise=(fornhit-32)*5/9;
+
+// document.write(fornhit+"ºF is "+celuise+("ºC"));
+
+// _______Q-07_________
+
+// var item1=650;
+
+// var quantity=3;
+
+// var item2=100;
+
+// var quanttity1=7;
+
+// var shiping=100;
+
+// var total= (item1*quantity)+(item2*quanttity1)+shiping;
+
+// document.write("The tota cost of your order is " +total);
+
+// ________Q-08___________
+
+// var marks=980;
+
+// var obtain=804;
+
+// var percent=(804/980*100);
+//  document.write("percentage: "+percent);
+
+// ___________Q-09________________
+
+// var Exchange=(104.80*10)+(25*28);
+
+// document.write("Total currency in PKR: "+Exchange);
+
+// ____________Q-10_____________
+
+// var number=6;
+
+// number=6+5*10/2;
+
+// console.log(number);
+
+// _______________Q-11_________________
+
+// document.write("<h1>Age Calculator</h1>");
+
+// var currentYear=2016;
+// var birthYear=1992;
+// var age= currentYear-birthYear;
+// document.write("current year: "+currentYear+"<br>"+"Birth Year: "+birthYear+"<br>"+"Your age: "+age);
+
+// ________________Q-12___________________
+
+// var raduis= 20;
+
+// var curmference= 2*3.142*20;
+// console.log(curmference);
+
+// var area= 3.142*400;
+// console.log(area);
+
+// ____________Q-13________________-
+
+// var snack ="choclate";
+
+// var age = 15;
+
+// var estimatedAge=65;
+// var perday=3;
+// var calculate= ((65-15)*3);
+
+// document.write("You will need "+calculate+" choclate chip to last until the ripe old age "+age);
+
+var a =10;
+a++
+console.log(a);
+
+a++;
+console.log(a);
+
+a--;
+console.log(a);
+
+a--;
+
+console.log(a);
+
+
+
+
+
+
+
+
+
+
