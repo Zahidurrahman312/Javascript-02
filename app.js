440e1e3ea94fa51e5a290fcc1cf93c9a6ff5e612
unknown <zahidrehamn886554@gmail.com>
2025-09-13 01:20:27 +0500

    Nested Array

---
+++ app.js
@@ -0,0 +1,53 @@
+console.log("Jvavascript connected!");
+
+// var student = [
+
+//     ["Ahmad","math","urdu"],
+
+//     ["Ali","english","physics"],
+
+//     ["Abas","bio"," urdu" ],
+
+// ];
+
+// console.log(student[0][0]);
+
+// var marks = [
+
+//         ["ali",[30,50,60]],
+
+//         ["abas", [50,60,80]],
+
+// ]
+
+// console.log("studet " + marks[0] [0] + "marks "+ marks[0][1]);
+
+var num =[
+
+    [1,2,4,5],
+
+    [6,7,8,9],
+
+    [10,11,12],
+
+
+];
+
+for(var i = 0; i < num.length; i++){
+
+    console.log(num[i]);
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
