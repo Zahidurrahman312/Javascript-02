020c276ee30a6b9fad6d2e9f6ea8dd6c033abfa1
unknown <zahidrehamn886554@gmail.com>
2025-09-14 02:23:43 +0500

    Nested loop

--- app.js
+++ app.js
@@ -22,28 +22,84 @@ console.log("Jvavascript connected!");
 
 // console.log("studet " + marks[0] [0] + "marks "+ marks[0][1]);
 
-var num =[
+// var num =[
 
-    [1,2,4,5],
+//     [1,2,4,5],
 
-    [6,7,8,9],
+//     [6,7,8,9],
 
-    [10,11,12],
+//     [10,11,12],
 
 
-];
+// ];
 
-for(var i = 0; i < num.length; i++){
+// for(var i = 0; i < num.length; i++){
 
-    console.log(num[i]);
+//     console.log(num[i][0]);
     
-}
+// }
+
+// for(var i = 1; i <= 20; i++){
+
+//     for(var j = 1; j <= i ; j++ ){
 
 
+//     }
+
+// console.log(i);
 
 
+// }
 
 
+// var str = 0;
+
+// for(var i = 1; i <= 5; i++){
+
+//     str += " *";
+
+
+//     console.log(str);
+    
+
+// }
+
+// var cities = ["karachi","lahore","quetta","islamabad"];
+
+// var cityCheck = prompt("Enter your city");
+
+// var isFound = false;
+
+// for(var i=0; i <cities.length; i++){
+
+
+//     if(cityCheck===cities[i]){
+
+//             isFound = true;
+
+
+//         console.log("Welcome");
+
+//         break;
+        
+//     }
+// }
+
+// if(isFound === false){
+
+//     console.log("Try again");
+    
+// }
+
+// var num = +prompt("Enter a number ");
+
+// var user = +prompt("How many time do you multiply");
+
+// for(var i= 1; i <= user; i++ ){
+
+//     document.write(num + "x" + i + "="+ (num*i)+ "<br />");
+    
+// }
 
 
 
